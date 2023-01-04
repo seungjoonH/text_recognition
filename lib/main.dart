8552967f564f6fac8216cb16de661aca9180f8d9
Bokwon Choi <89948250+Vincenchoi@users.noreply.github.com>
2023-01-04 16:56:01 +0900

    도로명주소 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:text_recognition/home.dart';
+import 'package:text_recognition/public_api.dart';
 
 import 'geocoding.dart';
 
@@ -21,7 +22,8 @@ class MyApp extends StatelessWidget {
       ),
       home: const HomePage(),
       routes: {
-        '/geolocation' : (context) => Geocoding()
+        '/geolocation' : (context) => Geocoding(),
+        '/publicApi' : (context) => PublicAPI(),
       },
     );
   }
